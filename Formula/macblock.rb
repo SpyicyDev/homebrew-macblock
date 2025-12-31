00603758a85437834508d8b4240ba24afaaedade
Mack Haymond <mack.haymond@icloud.com>
2025-12-31 11:34:39 -0500

    macblock v0.2.9
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/macblock.rb
+++ Formula/macblock.rb
@@ -7,8 +7,8 @@ class Macblock < Formula
   desc "Local DNS sinkhole for macOS using dnsmasq"
   homepage "https://github.com/SpyicyDev/macblock"
 
-  url "https://github.com/SpyicyDev/macblock/archive/refs/tags/v0.2.8.tar.gz"
-  sha256 "996c04e68f45058ac27dd77f09c6c141d2bd7482a410a2fbe40ec70c3b3b19da"
+  url "https://github.com/SpyicyDev/macblock/archive/refs/tags/v0.2.9.tar.gz"
+  sha256 "422e588452b4c81478444dbfe5e7504b1cd3261a3aa2371ec209991770e663ff"
   license "MIT"
 
   head "https://github.com/SpyicyDev/macblock.git", branch: "main"
